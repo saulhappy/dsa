fd49d7b005d9124671dc1e392f8c0f319515a073
Saul Feliz <646634@gmail.com>
2022-09-15 10:09:25 -0500

    add another method to sock merchant

--- sock_merchant.py
+++ sock_merchant.py
@@ -12,3 +12,10 @@ def sock_merchant(arr, n):
         pairs += value // 2
     
     return pairs
+
+
+# ANOTHER, MORE PYTHONIC METHOD USING COLLECTIONS
+from collections import Counter
+return sum(sock // 2 for sock in Counter(arr).values())
+
+print(sock_merchant([1,2,1,2,1,3,2], 7))
