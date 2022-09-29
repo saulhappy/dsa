8718b411174204692459a30fbf5bf9cc83e66834
Saul Feliz <646634@gmail.com>
2022-09-29 15:55:31 -0500

    add get_all_substrings function

---
+++ get_all_substrings.py
@@ -0,0 +1,14 @@
+"""
+For a giving string, get all the substring combinations.
+
+"""
+
+def get_all_substrings(s):
+    result = []
+    for i in range(len(s)):
+        for j in range(i, len(s)):
+            result.append(s[i:j+1])
+    return result
+
+
+print(get_all_substrings("abc"))
