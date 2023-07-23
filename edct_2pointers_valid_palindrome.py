37d6e2860b3d014afae4cde5db13340635227755
Saul Feliz <saul.feliz@cart.com>
2023-07-23 12:41:47 -0500

    add space/time complexity analysis

--- edct_2pointers_valid_palindrome.py
+++ edct_2pointers_valid_palindrome.py
@@ -30,3 +30,13 @@ print(is_valid_palindrome("RACECAR")) # => True
 print(is_valid_palindrome("saul")) # => False
 
 print(is_valid_palindrome("RACEACAR")) # => False
+
+
+"""
+Time Complexity:
+The time complexity is O(n) where n is the number of chars in the string. But this algo will run at O(n/2)
+because two pointers traversing each other don't have to traverse entire string, and meet at the middle.
+
+Space Complexity:
+O(1) in order to store our two indexes.
+"""
