7e1e6ae223f8fe8d2167331e49c050bdcb087bfd
Saul Feliz <saul.feliz@cart.com>
2023-07-25 09:50:19 -0500

    add educative find_sum_of_three problem

---
+++ edct_2pointers_sum_of_three_values.py
@@ -0,0 +1,20 @@
+
+def find_sum_of_three(nums, target):
+   return False
+
+
+
+
+
+
+
+
+# TESTS
+
+print(find_sum_of_three(nums=[2, 3, 1], target=6)) # => True
+
+print(find_sum_of_three(nums=[1, -1, 1], target=2)) # => False
+
+print(find_sum_of_three(nums=[1, 2, 4, 6, 8, 20], target=31)) # => False
+
+print(find_sum_of_three(nums=[1, 3, 4, 6, 8, 20], target=31)) # => True
