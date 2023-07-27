0b4c76f0007e17ebe1a88a03b2af27add740bcb4
Saul Feliz <saul.feliz@cart.com>
2023-07-27 09:28:28 -0500

    add educative 2 pointer sort_colors problem

---
+++ edct_2pointers_sort_colors.py
@@ -0,0 +1,11 @@
+def sort_colors(colors):
+    return colors
+
+
+# TEST CASES
+
+print(sort_colors(colors=[2, 2, 1, 1, 0])) # => [0, 1, 1, 2, 2]
+
+print(sort_colors(colors=[2, 2, 0, 1, 2, 2, 0, 2])) # => [2, 2, 0, 1, 2, 2, 0, 2]
+
+print(sort_colors(colors=[0, 0, 1, 0, 1, 1, 1, 2, 1, 2])) # => [0, 0, 0, 1, 1, 1, 1, 1, 2, 2]
