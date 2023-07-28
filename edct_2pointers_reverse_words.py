ed174d9ce4b99aa6287443edfe39635896c9514a
Saul Feliz <saul.feliz@cart.com>
2023-07-28 10:52:08 -0500

    add reverse_words two pointer problem

---
+++ edct_2pointers_reverse_words.py
@@ -0,0 +1,11 @@
+def reverse_words(sentence):
+
+   # Replace this placeholder return statement with your code
+
+   return ""
+
+print(reverse_words("Educative Answers")) # => # Answers Educative
+
+print(reverse_words("Hurray 3 2 1")) # => # 1 2 3 Hurray
+
+print(reverse_words("practice problems to improve your coding")) # => # coding your improve to problems practice
