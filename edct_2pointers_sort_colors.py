bf5f880bfff93c60450cd951c1f0277b2e484be9
Saul Feliz <saul.feliz@cart.com>
2023-07-28 10:46:14 -0500

    solve 2 pointer sort colors problem

--- edct_2pointers_sort_colors.py
+++ edct_2pointers_sort_colors.py
@@ -1,4 +1,21 @@
 def sort_colors(colors):
+    red = 0
+    white = 0
+    blue = len(colors) -1
+
+    for _ in range(len(colors)):
+        if colors[white] == 0:
+            colors[white], colors[red] = colors[red], colors[white]
+            red += 1
+            white += 1
+        
+        elif colors[white] == 1:
+            white += 1
+        
+        else:
+            colors[white], colors[blue] = colors[blue], colors[white]
+            blue -= 1
+    
     return colors
 
 
@@ -6,6 +23,6 @@ def sort_colors(colors):
 
 print(sort_colors(colors=[2, 2, 1, 1, 0])) # => [0, 1, 1, 2, 2]
 
-print(sort_colors(colors=[2, 2, 0, 1, 2, 2, 0, 2])) # => [2, 2, 0, 1, 2, 2, 0, 2]
+print(sort_colors(colors=[2, 2, 0, 1, 2, 2, 0, 2])) # => [0, 0, 1, 2, 2, 2, 2, 2]
 
 print(sort_colors(colors=[0, 0, 1, 0, 1, 1, 1, 2, 1, 2])) # => [0, 0, 0, 1, 1, 1, 1, 1, 2, 2]
