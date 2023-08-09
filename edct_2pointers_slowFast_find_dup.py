9b979cdaf1ad6c0dfee6e1e9578b62df194f810d
Saul Feliz <saul.feliz@cart.com>
2023-08-09 14:10:38 -0500

    add 2 pointer find_duplicate problem

---
+++ edct_2pointers_slowFast_find_dup.py
@@ -0,0 +1,17 @@
+def find_duplicate(nums):
+    
+    return 0
+
+
+
+# TEST CASES:
+
+print(find_duplicate([1, 5, 4, 3, 2, 4, 6])) # => 4
+
+print(find_duplicate([5, 6, 1, 3, 7, 3, 2, 4, 8])) # => 3
+
+print(find_duplicate([2, 10, 6, 7, 11, 7, 9, 4, 3, 8, 5, 1])) # => 7
+
+print(find_duplicate([9, 6, 12, 7, 15, 8, 3, 13, 2, 10, 4, 14, 1, 11, 9, 5])) # => 9
+
+
