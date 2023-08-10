b674a4eb203eac60dfe2c81fa3cbb921e8c32222
Saul Feliz <saul.feliz@cart.com>
2023-08-10 15:06:34 -0500

    add find_repeated_sequences problem

---
+++ edct_sliding_window_find_repeated_sequences.py
@@ -0,0 +1,12 @@
+def find_repeated_sequences(s, k):
+
+    # Replace this placeholder return statement with your code
+
+    return set()
+
+
+print(find_repeated_sequences(s="AGCTGAAAGCTTAGCTG", k=5)) # => ("AGCTG")
+
+print(find_repeated_sequences(s="AGAGCTCCAGAGCTTG", k=6)) # => ("AGAGCT")
+
+print(find_repeated_sequences(s="ATATATATATATATAT", k=6)) # => ("TATATA", "ATATAT")
