b46713cf2b1d9ff0f47c0a1211c6c8a37caf7e71
Saul Feliz <saul.feliz@cart.com>
2023-08-16 16:28:11 -0500

    update

--- edct_sliding_window_find_max_nums.py
+++ edct_sliding_window_find_max_nums.py
@@ -1,10 +1,11 @@
 """
 Given an integer list, nums, find the maximum values in all the contiguous subarrays (windows) of size w.
 """
-from collections import deque
+
+# O(n x w) solution. For an O(n) solution: https://leetcode.com/problems/sliding-window-maximum/solutions/3915849/fast-o-n-deque/
 
 def find_max_sliding_window(nums, w):
-    result = deque()
+    result = []
 
     for i in range(len(nums)):
         curr_window = nums[i:w]
@@ -16,6 +17,10 @@ def find_max_sliding_window(nums, w):
 
 
 # TEST CASES
+
+print(find_max_sliding_window(nums=[1,3,-1,-3,5,3,6,7], w=3)) # => [5, 5, 4, 4, 6, 7, 20]
+
+print(find_max_sliding_window(nums=[1], w=1)) # => [5, 5, 4, 4, 6, 7, 20]
 
 print(find_max_sliding_window(nums=[-4, 5, 4, -4, 4, 6, 7, 20], w=2)) # => [5, 5, 4, 4, 6, 7, 20]
 
