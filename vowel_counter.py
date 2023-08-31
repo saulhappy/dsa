a4926daadcddeedce3876b894349d58ae7c8e4e5
Saul Feliz <saul.feliz@cart.com>
2023-08-31 09:23:03 -0500

    trying another unit test method

--- vowel_counter.py
+++ vowel_counter.py
@@ -3,7 +3,36 @@ def vowel_count(word):
         + word.count("o") + word.count("u")
 
 
-print(vowel_count("helicopter")) # -> 4
-print(vowel_count("saul")) # -> 2
-print(vowel_count("aaeeoo")) # -> 6
-print(vowel_count("cdt")) # -> 0
+# print(vowel_count("helicopter")) # -> 4
+# print(vowel_count("saul")) # -> 2
+# print(vowel_count("aaeeoo")) # -> 6
+# print(vowel_count("cdt")) # -> 0
+
+
+test1 = vowel_count("helicopter")
+expected1 = 4
+if test1 == expected1:
+    print("Pass")
+else:
+    print(f"Fail - expected: {expected1}, got: {test1}")
+
+test2 = vowel_count("Saul")
+expected2 = 2
+if test1 == expected1:
+    print("Pass")
+else:
+    print(f"Fail - expected: {expected1}, got: {test1}")
+
+test3 = vowel_count("aaeeoo")
+expected3 = 6
+if test1 == expected1:
+    print("Pass")
+else:
+    print(f"Fail - expected: {expected1}, got: {test1}")
+
+test4 = vowel_count("cdt")
+expected4 = 0
+if test1 == expected1:
+    print("Pass")
+else:
+    print(f"Fail - expected: {expected1}, got: {test1}")
